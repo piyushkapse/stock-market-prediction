83cdb552d9e1145e481be30f213b7df971e8c0a3
piyush kapse <piyushh.kapse@gmail.com>
2019-06-02 19:02:15 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,12 @@
+
+
+
+
+consumer_key = 'CUUbCMp3WCUGMbjTkIurFKUqU'
+consumer_secret = 'Rd1dQCRcF5j0NEgXDfZQV2KVjAEIuLYoFWjoG6ckzW71q455ef '
+access_token =  '1911313218-gmWnXVfjoJIHBQ55zqIpVI0i4IPzbTeAzeHpKW4'
+access_token_secret = 'A6OyfvbHcgJmUb8MZ2ljnnqcqb5dvujcNrdmREEWrJhJd'
+
+num_of_tweets = int()
+
+
